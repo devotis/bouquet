9b392a069d5e3cdbe739b56e6592634c0f540773
Christiaan Westerbeek <chris@devotis.nl>
2020-04-26 22:13:47 +0200

    Test current settings returned plus current_user set by queryWithContext

--- test/pg.js
+++ test/pg.js
@@ -18,7 +18,6 @@ tape('logger', async t => {
     const url = 'https://example.com/user/42';
     const q = {
         string: 'simple',
-        datum: new Date(),
         zero: 0,
         number: 1,
         bool: true,
@@ -43,7 +42,7 @@ tape('logger', async t => {
     });
 
     const sql = [
-        'SELECT now() as "now"',
+        'SELECT current_user',
         ...Object.keys(q).map(
             key =>
                 `, current_setting('request.query.${key.replace(
@@ -55,44 +54,23 @@ tape('logger', async t => {
 
     const result = await queryWithContext(getRequest, getRole, sql);
 
-    // const settings = getSettingsFromRequest(
-    //     getRequest,
-    //     ['headers', 'query', 'body', 'session'],
-    //     { timezone: 'Europe/Amsterdam' }
-    // );
-    //
-    // console.log(settings);
-    //
-    // const sqlSettingsQuery = getSqlSettingsQuery(settings);
-    //
-    // console.log(sqlSettingsQuery);
-    //
-    // const result = await query('SELECT now() as nu', []);
-    //
-    // const [pgClient, release] = await getClient();
-    // await pgClient.query('begin');
-    //
-    // try {
-    //     // If there is at least one local setting, load it into the database.
-    //     await pgClient.query(sqlSettingsQuery);
-    //
-    //     const result = await pgClient.query(
-    //         `SELECT now() as nu, current_setting('request.header.x-request-id') as nu2`
-    //     );
-    //     console.log(result);
-    // } catch (err) {
-    //     console.error();
-    // } finally {
-    //     // Cleanup our Postgres client by ending the transaction and releasing
-    //     // the client back to the pool. Always do this even if the query fails.
-    //     try {
-    //         await pgClient.query('commit');
-    //     } finally {
-    //         release();
-    //     }
-    // }
-
-    console.log(result.rows);
+    t.deepEqual(result.rows, [
+        {
+            current_user: 'app_anonymous',
+            string: 'simple',
+            zero: '0',
+            number: '1',
+            bool: 'true',
+            bool2: 'false',
+            obj: '{"x":"123"}',
+            emptyobj: '{}',
+            arr: '{"1","2","3"}',
+            emptyarr: '{}',
+            nul: '',
+            undef: '',
+            "inje'ction": "Isn't injected",
+        },
+    ]);
 
     t.end();
 });
